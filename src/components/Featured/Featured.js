d644fdc22e0fc40f71faa180474754a03df19f0b
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-06-24 14:22:57 -0700

    set up multiple components

---
+++ src/components/Featured/Featured.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Featured = () => {
+    return (
+        <div>
+            <h1 className="text-black text-3xl">Featured</h1>
+        </div>
+    )
+}
+
+export default Featured
---
+++ src/components/Page/Page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const Page = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Page;
