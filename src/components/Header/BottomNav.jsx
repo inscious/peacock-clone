94d84e47202e150f72adcdae22768bc72f816608
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-07-28 18:02:38 -0700

    fully responsive navigation to any device

--- src/components/Header/BottomNav.jsx
+++ src/components/Header/BottomNav.jsx
@@ -2,17 +2,40 @@ import React from "react";
 
 const BottomNav = () => {
     return (
-        <div className="bottom-nav-container">
-            {/* <div className="flex flex-row"> */}
-                <ul classname="flex">
-                    <li className="flex">
-                        <a className="text-sm">Channels</a>
-                    </li>
-                    <li className="flex">
-                        <a className="text-sm">Browse</a>
-                    </li>
-                </ul>
-            {/* </div> */}
+        <div className="bottom-nav-container lg:hidden">
+            <div className="flex flex-row w-full justify-center">
+                <div className="flex items-center w-12 h-12 justify-center rounded-full overflow-hidden bg-[#222222] cursor-pointer">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="white"
+                        strokeWidth={2}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                    </svg>
+                </div>
+                <div className="bg-[#222222] rounded-full h-12 flex justify-center items-center mx-4">
+                    <span className="text-white flex justify-center items-center py-3 px-3 md:px-5 duration-200 cursor-pointer ">
+                        <a className="text-xs md:text-base duration-200 hover:border-b-4">
+                            Channels
+                        </a>
+                    </span>
+                    <span className="text-black flex bg-gradient-to-r from-[#fab503] to-[#fe9001] rounded-full py-4 px-[18px] md:px-5 md:py-3 justify-center items-center duration-200 cursor-pointer">
+                        <a className="text-xs md:text-base duration-200">
+                            Browse
+                        </a>
+                    </span>
+                </div>
+                <div className="flex items-end w-12 h-12 rounded-full bg-[#222222] cursor-pointer duration-200 hover:bg-gradient-to-r hover:from-[#eba23f] hover:to-[#f2bf75]">
+                    <img src="https://imageservice.disco.peacocktv.com/pcms/7fdf0012-3001-434a-be15-9ea2d3c32275/AGG_SOURCE/400?territory=US&proposition=NBCUOTT&language=eng" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { navData } from "./navData";
 import logoMobile from "../../assets/mobile.png";
+import dsktpLogo from "../../assets/desktop.png";
 
 import BottomNav from "./BottomNav";
 
 const Header = () => {
     return (
         <div className="header-container">
-            <div className="image-container">
+            <div className="image-container-mobile">
                 <img src={logoMobile} className="mobile-logo" />
+            </div>
+            <div className="image-container-desktop">
+                <img
+                    src={dsktpLogo}
+                    className="hidden lg:flex h-12 absolute left-8 top-9 xl:left-16 duration-200"
+                />
             </div>
             <div className="flex">
                 <ul className="nav-links-wrapper">
@@ -22,6 +29,53 @@ const Header = () => {
                         );
                     })}
                 </ul>
+            </div>
+            <div className="hidden lg:flex w-full justify-center">
+                <div className="bg-[#222222] rounded-full h-12 flex justify-center items-center -mr-6 z-10">
+                    <span className="text-white flex justify-center items-center py-4 pl-4 mr-1 xl:mr-0 xl:px-6 duration-200 cursor-pointer">
+                        <a className="text-sm duration-200 hover:border-b-4 xl:text-base">
+                            Channels
+                        </a>
+                    </span>
+                    <span className="text-black flex bg-gradient-to-r from-[#fab503] to-[#fe9001] rounded-full p-4 xl:px-6 justify-center items-center duration-200 cursor-pointer h-12">
+                        <a className="text-sm duration-200 xl:text-base">Browse</a>
+                    </span>
+                </div>
+                <ul className="flex bg-[#222222] rounded-r-full pl-6">
+                    {navData.map((data) => {
+                        return (
+                            <li className="flex items-center mx-2.5 xl:mx-[1.125rem]">
+                                <a
+                                    className="text-white text-sm opacity-60 hover:opacity-100 duration-200 xl:text-base"
+                                    href={data.url}
+                                >
+                                    {data.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="hidden lg:flex absolute right-8 xl:right-16 duration-200">
+                <div className="flex items-center w-12 h-12 justify-center rounded-full overflow-hidden bg-[#222222] cursor-pointer mx-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="white"
+                        strokeWidth={2}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                    </svg>
+                </div>
+                <div className="flex items-end w-12 h-12 rounded-full bg-[#222222] cursor-pointer duration-200 hover:bg-gradient-to-r hover:from-[#eba23f] hover:to-[#f2bf75]">
+                    <img src="https://imageservice.disco.peacocktv.com/pcms/7fdf0012-3001-434a-be15-9ea2d3c32275/AGG_SOURCE/400?territory=US&proposition=NBCUOTT&language=eng" />
+                </div>
             </div>
             <BottomNav />
         </div>
