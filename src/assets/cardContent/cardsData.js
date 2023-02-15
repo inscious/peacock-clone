44f38f2836b4f497569054dfade6fffbc92e584f
Saul Cortes <92839820+inscious@users.noreply.github.com>
2023-02-14 19:27:31 -0800

    added hover effect on card component

--- src/assets/cardContent/cardsData.js
+++ src/assets/cardContent/cardsData.js
@@ -9,7 +9,7 @@ const myStuff = [
     {
         title: "The Office: Superfan Episodes",
         genre: "Comedy",
-        seasons: "6",
+        seasons: "6 Seasons",
         tomato: "%",
         img: "https://imageservice.disco.peacocktv.com/pcms/c84c0346-9c16-11ed-a186-2faeccdc9245/LAND_16_9/400?territory=US&proposition=NBCUOTT&language=eng",
     },
@@ -24,42 +24,42 @@ const myStuff = [
 
 const peacockPicks = [
     {
-        name: "The Traitors",
+        title: "The Traitors",
         genre: "Competition",
         seasons: "1 Season",
-        tomato: "%",
+        tomato: "95%",
         img: "https://imageservice.disco.peacocktv.com/uuid/d31d5fdb-ec8d-394b-9ab3-6cf3418795d0/TITLE_ART_16_9/400?language=eng&territory=US&proposition=NBCUOTT&version=05ce5d43-04db-3196-987f-b414a5d5e52c",
     },
     {
-        name: "Chucky",
+        title: "Chucky",
         genre: "Horror",
         seasons: "1 Season",
         tomato: "91%",
         img: "https://imageservice.disco.peacocktv.com/uuid/f2c74a87-d049-3e74-b01b-8e09660cc838/TITLE_ART_16_9/400?language=eng&territory=US&proposition=NBCUOTT&version=3d40b6bc-1a1c-3e3b-b876-690c510e2713&personaid=e2ce4e1482c298af5c91a780ca4d6511cade82942b7b16265f7fe7e266b9d1e6&userid=1dcb0d2fbd343c905e34deed51364eea518cc9d1&contentid=&pageidentifier=browse&rail=peacock-picks&device=computer/pc",
     },
     {
-        name: "Resident Alien",
+        title: "Resident Alien",
         genre: "Sci-Fi, Comedy",
         seasons: "2 Seasons",
         tomato: "94%",
         img: "https://imageservice.disco.peacocktv.com/pcms/93b2f39a-197d-11ed-a515-b781d0b2cb15/LAND_16_9/400?territory=US&proposition=NBCUOTT&language=eng&personaid=e2ce4e1482c298af5c91a780ca4d6511cade82942b7b16265f7fe7e266b9d1e6&userid=1dcb0d2fbd343c905e34deed51364eea518cc9d1&contentid=&pageidentifier=browse&rail=peacock-picks&device=computer/pc",
     },
     {
-        name: "Killer Couples",
+        title: "Killer Couples",
         genre: "Documentary",
         seasons: "16 Seasons",
         tomato: "",
         img: "https://imageservice.disco.peacocktv.com/uuid/faf1d521-1e0a-311e-a89b-b541a200f247/TITLE_ART_16_9/400?language=eng&territory=US&proposition=NBCUOTT&version=68fbafbe-898a-3a48-af15-48b2c0ccd7b8&personaid=e2ce4e1482c298af5c91a780ca4d6511cade82942b7b16265f7fe7e266b9d1e6&userid=1dcb0d2fbd343c905e34deed51364eea518cc9d1&contentid=&pageidentifier=browse&rail=peacock-picks&device=computer/pc",
     },
     {
-        name: "The Resort",
+        title: "The Resort",
         genre: "Comedy-Drama",
         seasons: "1 Season",
         tomato: "87%",
         img: "https://imageservice.disco.peacocktv.com/pcms/a8eb9606-0e77-11ed-be98-c35c91ce68b5/LAND_16_9/400?territory=US&proposition=NBCUOTT&language=eng&personaid=e2ce4e1482c298af5c91a780ca4d6511cade82942b7b16265f7fe7e266b9d1e6&userid=1dcb0d2fbd343c905e34deed51364eea518cc9d1&contentid=&pageidentifier=browse&rail=peacock-picks&device=computer/pc",
     },
     {
-        name: "That 70's Show",
+        title: "That 70's Show",
         genre: "Comedy",
         seasons: "8 Seasons",
         tomato: "91%",
--- src/components/CardContent/Card.jsx
+++ src/components/CardContent/Card.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { SwiperSlide } from "swiper/react";
+import { rotten } from "../Featured/data";
 
 const Card = ({ data, index }) => {
     return (
         // <SwiperSlide>
         <div
             key={index}
-            className="group cursor-pointer duration-150 hover:scale-105"
+            className="group cursor-pointer overflow-hidden rounded-xl duration-150 hover:scale-105"
         >
             <img
                 src={data.img}
-                className="aspect-video rounded-xl object-cover z-10"
+                className="absolute aspect-video rounded-xl object-cover z-10"
             />
-            {/* <div className="h-full w-full bg-amber-900 z-50 aspect-video rounded-xl relative" /> */}
+            <div className="group-hover:z-20 relative top-[100%] aspect-video h-full w-full bg-gradient-to-t from-black/75 duration-500 group-hover:top-0">
+                <div className="absolute bottom-0 mx-auto flex w-full flex-col pl-4 pb-3">
+                    <span>
+                        <p className="whitespace-nowrap text-sm tracking-tight text-white duration-150 xl:text-lg">
+                            {data.title}
+                        </p>
+                    </span>
+                    <span className="flex items-center text-xs text-white">
+                        <img src={rotten} className="mr-2 h-4" />
+                        <p className="flex items-center">
+                            {data.tomato}
+                            <span className="mx-2 text-[.5rem]">•</span>
+                        </p>
+                        <p className="flex items-center">
+                            {data.genre}
+                            <span className="mx-2 text-[.5rem]">•</span>
+                        </p>
+                        <p className="flex items-center">
+                            {data.seasons || data.released}
+                        </p>
+                    </span>
+                </div>
+            </div>
         </div>
         // </SwiperSlide>
     );
